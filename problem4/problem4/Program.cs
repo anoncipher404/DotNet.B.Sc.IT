5ac2d1b80ad08d7203eed1ded9d7188f1c7ce695
anoncipher404 <133158027+anoncipher404@users.noreply.github.com>
2023-09-22 00:13:11 +0530

    Add files via upload

---
+++ problem4/problem4/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Problem Statement 4: Given a string, write a program to remove all the whitespaces from 
+the string.
+Example 1:
+Input: str = “Take you forward” 
+Output: Takeyouforward
+Explanation: After removing all the whitespaces Takeyouforward is the result
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem4
+{
+    internal class Program
+    {
+        static string RemWhiteSpace(string inp) 
+        { 
+            string output= "";
+            foreach(char ch in inp)
+            {
+                if (!char.IsWhiteSpace(ch))
+                {
+                    output += ch;
+                }   
+            }
+            return output;
+        }
+
+        static void Main(string[] args)
+        {
+            string inp = "Take you forward";
+            string outs = RemWhiteSpace(inp);
+            Console.WriteLine(outs);
+        }
+    }
+}
